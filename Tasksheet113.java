d00d25e0b3ce18270f9e293876655adff09125f3
BandojaNelmarieJoy <150577420+BandojaNelmarieJoy@users.noreply.github.com>
2023-12-03 16:48:56 +0800

    Add files via upload

---
+++ Tasksheet113.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasksheet1.pkg1.pkg3;
+
+/**
+ *
+ * @author NelJoy
+ */
+public class Tasksheet113 {
+
+   
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+
+        for (int i = 1; i <= check_number; i++) {
+            message = (i % 2 == 0) ? i + " is even number" : i + " is odd number";
+            System.out.println(message);
+        }
+    }
+}
